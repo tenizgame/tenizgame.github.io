56965df26eb13a8758774ac910e3177cfe69c0be
TENIZ <103454770+tenizgame@users.noreply.github.com>
2022-04-18 18:17:19 -0400

    Add files via upload

--- teniz.js
+++ teniz.js
@@ -2,14 +2,14 @@
 btnParty.addEventListener("click", () => {
     confetti("tsparticles", {
       angle: 90,
-      count: 30,
+      count: 250,
       position: { x: 50, y: 50 },
       spread: 90,
       startVelocity: 45,
       decay: 0.9,
       gravity: 1,
       drift: 0,
-      ticks: 100,
+      ticks: 200,
       colors: ["#fff", "#f00"],
       shapes: ["square", "circle"],
       scalar: 1,
