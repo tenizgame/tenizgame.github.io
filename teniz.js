c8b75913ba0f31ad2d7ceafe577235bd48c46336
TENIZ <103454770+tenizgame@users.noreply.github.com>
2022-04-18 18:29:51 -0400

    Add files via upload

--- teniz.js
+++ teniz.js
@@ -1,22 +1,22 @@
 //Confetti Begin
 btnParty.addEventListener("click", () => {
-    confetti("tsparticles", {
-      angle: 90,
-      count: 250,
-      position: { x: 50, y: 50 },
-      spread: 90,
-      startVelocity: 45,
-      decay: 0.9,
-      gravity: 1,
-      drift: 0,
-      ticks: 200,
-      colors: ["#fff", "#f00"],
-      shapes: ["square", "circle"],
-      scalar: 1,
-      zIndex: 2000,
-      disableForReducedMotion: true
-    });
+  confetti("tsparticles", {
+    angle: 90,
+    count: 250,
+    position: { x: 50, y: 50 },
+    spread: 90,
+    startVelocity: 60,
+    decay: 0.9,
+    gravity: 1,
+    drift: 0,
+    ticks: 200,
+    colors: ["#fff", "#f00"],
+    shapes: ["square", "circle"],
+    scalar: 1,
+    zIndex: 2000,
+    disableForReducedMotion: true
   });
+});
 //Confetti End
 //Local Storage Initial Setting
 window.localStorage;
