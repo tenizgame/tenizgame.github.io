3810e5e8b9b52313bf91ebb8c5495b34e400787d
TENIZ <103454770+tenizgame@users.noreply.github.com>
2022-04-25 02:50:40 -0400

    Add files via upload

--- teniz.js
+++ teniz.js
@@ -269,7 +269,7 @@ function displayFooter() {
 }
 //Baseline Date
 var a = new Date(); // Current date now.
-var b = new Date(2022, 2, 4, 0, 0, 0, 0); // Start of TENIZ.
+var b = new Date(2022, 3, 11, 0, 0, 0, 0); // Start of TENIZ.
 var d = (a - b); // Difference in milliseconds.
 var days = parseInt((d / 1000) / 86400);
 if (localStorage.getItem('gameover' + days) != 0 && localStorage.getItem('gameover' + days) != 1) {
